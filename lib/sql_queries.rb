4c441d7f7af69b20094eb1428a0d21b1b03d180c
Mike Dilley <mike.dilley@gmail.com>
2018-11-27 17:24:46 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -37,7 +37,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"SELECT Projects.category, SUM(Pledges.amount) FROM Projects
+"SELECT Projects.category, Pledges.amount FROM Projects
  INNER JOIN Pledges ON Projects.id = Pledges.project_id
  WHERE Projects.category = \"music\";
 "
