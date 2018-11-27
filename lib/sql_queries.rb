bba4d92306d0d509790851b846157bef18a4b6b6
Mike Dilley <mike.dilley@gmail.com>
2018-11-27 16:55:09 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,7 +21,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT Projects.title, Projects.funding_goal FROM Projects
+"SELECT Projects.title, Pleges.amount FROM Projects
  INNER JOIN Pledges ON Projects.id = Pledges.project_id
  GROUP BY Projects.title
  HAVING SUM(Pledges.amount) > Projects.funding_goal
